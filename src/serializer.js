bd53440941b999738c9c85e93987ce3ceb93eb03
Sebastian <13647606+H34D@users.noreply.github.com>
2025-08-12 14:58:26 +0200

    chore: renovate

--- src/serializer.js
+++ src/serializer.js
@@ -1,71 +1,64 @@
 export default class Serializer {
+  static matchSymbols(obj) {
+    const regEx = /^\$\$Symbol:(.*)$/;
 
-    static matchSymbols(obj) {
-        const regEx = /^\$\$Symbol:(.*)$/;
+    return obj && obj.match && obj.match(regEx);
+  }
 
-        return obj && obj.match && obj.match(regEx)
+  static replaceSymbol(key, obj) {
+    const match = Serializer.matchSymbols(key);
+
+    if (match) {
+      obj[Symbol.for(match[1])] = obj[key];
+      delete obj[key];
     }
 
-    static replaceSymbol(key, obj) {
-        const match = Serializer.matchSymbols(key);
+    return obj;
+  }
 
-        if (match) {
-            obj[Symbol.for(match[1])] = obj[key];
-            delete obj[key];
+  static replaceSymbols(obj) {
+    for (const key of Object.keys(obj)) {
+      obj = Serializer.replaceSymbol(key, obj);
+
+      if (obj[key] && typeof obj[key] === "object") {
+        Serializer.replaceSymbols(obj[key]);
+      }
+    }
+  }
+
+  static serialize({ obj, compress = false } = {}) {
+    const replacer = (name, value) => {
+      if (typeof value === "symbol") {
+        value = `$$Symbol:${Symbol.keyFor(value)}`;
+      }
+
+      if (
+        value &&
+        typeof value === "object" &&
+        Object.getOwnPropertySymbols(value).length > 0
+      ) {
+        value = Object.assign({}, value);
+        for (const symbol of Object.getOwnPropertySymbols(value)) {
+          value[`$$Symbol:${Symbol.keyFor(symbol)}`] = value[symbol];
         }
+      }
 
-        return obj;
-    }
+      return value;
+    };
 
-    static replaceSymbols(obj) {
-        for (const key of Object.keys(obj)) {
+    return JSON.stringify(obj, replacer, compress ? 0 : 2);
+  }
 
-            obj = Serializer.replaceSymbol(key, obj);
+  static deserialize(json) {
+    const reviver = (name, value) => {
+      const valueMatches = Serializer.matchSymbols(value);
+      return valueMatches ? Symbol.for(valueMatches[1]) : value;
+    };
 
-            if (obj[key] &&
-                typeof obj[key] === "object") {
-                Serializer.replaceSymbols(obj[key]);
-            }
-        }
-    }
+    let obj = JSON.parse(json, reviver);
 
-    static serialize(
-        {
-            obj,
-            compress = false
-        } = {}
-    ) {
-        const replacer = (name, value) => {
-            if (typeof value === "symbol") {
-                value = `$$Symbol:${Symbol.keyFor(value)}`
-            }
+    Serializer.replaceSymbols(obj);
 
-            if (value &&
-                typeof value === "object" &&
-                Object.getOwnPropertySymbols(value).length > 0) {
-
-                value = Object.assign({}, value);
-                for (const symbol of Object.getOwnPropertySymbols(value)) {
-                    value[`$$Symbol:${Symbol.keyFor(symbol)}`] = value[symbol]
-                }
-            }
-
-            return value
-        };
-
-        return JSON.stringify(obj, replacer, compress ? 0 : 2);
-    }
-
-    static deserialize(json) {
-        const reviver = (name, value) => {
-            const valueMatches = Serializer.matchSymbols(value);
-            return valueMatches ? Symbol.for(valueMatches[1]) : value
-        };
-
-        let obj = JSON.parse(json, reviver);
-
-        Serializer.replaceSymbols(obj);
-
-        return obj
-    }
+    return obj;
+  }
 }
